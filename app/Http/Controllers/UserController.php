58a5abfe2abad9082127c64b4a7ff04ccdc98850
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2022-04-16 04:03:13 +0000

    会員登録(簡易)を追加

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRegisterPost;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User as UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * トップページ を表示する
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('user.register');
+    }
+
+    /**
+     * ユーザーの新規登録
+     */
+    public function register(UserRegisterPost $request)
+    {
+        // validate済みのデータの取得
+        $datum = $request->validated();
+        $datum['password'] = Hash::make($datum['password']);
+        //
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
+
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
+
+        // テーブルへのINSERT
+        try {
+            $r = UserModel::create($datum);
+        } catch(\Throwable $e) {
+            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
+
+        // タスク登録成功
+        $request->session()->flash('front.user_register_success', true);
+
+        //
+        return redirect('/');
+    }
+}
